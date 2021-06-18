051367521307eaffae8f0be27fb7ff14beed867f
Tim Kuipers <t.kuipers@ultimaker.com>
2021-06-18 17:14:10 +0200

    some small fixes to matlab code

--- analytical_optimization/straight/grid_search.m
+++ analytical_optimization/straight/grid_search.m
@@ -1,17 +1,21 @@
+clear constants; % otherwise changes to the script aren't loaded until you restart MATLAB
 constants;
+clear design_variables;
 design_variables;
+clear objective;
 objective;
+clear constraints;
 constraints;
 
 Nwb = 45;
 Nva = 45;
 Nhf = 13;
-NF = 100;
+NF = 50;
 
 wbs = linspace(2 * wb_min, w_max - 2 * wa_min, Nwb);
 vas = linspace(wa_min, w_max - wb_min, Nva);
 hfs = linspace(h_min, h_max - h_min, Nhf);
-Fs = linspace(1, 30, NF);
+Fs = linspace(10, 30, NF);
 
 obj_min = 9999999;
 best_x = [-1,-1,-1,-1]; % placeholder
@@ -21,10 +25,19 @@ for wb_ = wbs
         for hf_ = hfs
             for F_ = Fs
                 x = [wb_, va_, hf_, F_];
-                constraints = [gwb(x), gva(x), gvb(x), ghf(x), gd(x), gta(x), gtb(x), gca(x), gzb(x)];
+                constraints = ...
+                    [ gwb(x)
+                      gva(x)
+                      gvb(x)
+                      ghf(x)
+                      gd(x)
+                      gta(x)
+                      gtb(x)
+                      gca(x)
+                      gzb(x)];
                 constraints_are_valid = true;
                 for g = constraints
-                    constraints_are_valid = constraints_are_valid && g <= 0.0001;
+                    constraints_are_valid = constraints_are_valid & (g <= 0.0001);
                 end
                 if constraints_are_valid
                     obj = f(x);
@@ -39,8 +52,8 @@ for wb_ = wbs
     end
 end
 
-fprintf('The minimum objective of %f is reached for: \n', obj_min)
-fprintf('The stress is %f \n', 1 / obj_min)
+x = best_x;
+fprintf('The minimum objective of %f with a max nominal stress of %f is reached for: \n', obj_min, 1 / obj_min)
 fprintf('w_b* = %f \n', wb(x));
 fprintf('v_a* = %f \n', va(x));
 fprintf('h_f* = %f \n', hf(x));
