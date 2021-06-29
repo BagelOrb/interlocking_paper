380a744c046e214d27461a1b9cc640e71b8f97b2
Tim Kuipers <t.kuipers@ultimaker.com>
2021-06-29 11:04:59 +0200

    use active set of constraints

--- analytical_optimization/SQP_diagonal/SQP_glob.m
+++ analytical_optimization/SQP_diagonal/SQP_glob.m
@@ -16,17 +16,18 @@ constraints_sqp;
 %       order derivatives of ddhdxx
 
 syms wa wb L F
-syms l1 l2 l3 l4
+syms l1 l2 l3 l4 l5 l6 l7 l8 l9 l10
 
 % Define starting point
 x = [wa; wb; L; F];
-lambda = [l1; l2; l3; l4]; % is needed for symbolic different
+lambdas = [l1; l2; l3; l4; l5; l6; l7; l8; l9; l10]; % is needed for symbolic different
 
 % Set index of active constraints
-h_idx = [1; 5; 8; 9];
+h_idx = [];
+lambda_k = [];
 
 % Set number of iterations
-Niter = 100;       % Max number of iterations
+Niter = 25;
 
 f_sym = sym(f);
 
@@ -35,7 +36,7 @@ nh = length(h_idx);
 nx = length(x);
 
 x_k = ones(1,nx);
-lambda_k = ones(1,nx);
+lambdas_k = ones(1,ng);
 
 delta = 100;
 options = optimset('Display', 'off');
@@ -66,18 +67,20 @@ end
 
 for p = 1:Niter
     % Determine active constraints
-    q = 1;
+    lambdas_k(h_idx) = lambda_k;
+    g_k = eval(subs(g, x.', x_k));
+    indices = [1:ng]; % set all violated constraints as active
+    h_idx = [indices(g_k > .000001)];
+    nh = length(h_idx);
+    h = g(h_idx);
+    lambda = lambdas(h_idx); % change subset of lambdas to match subset of active constraints
+    lambda_k = lambdas_k(h_idx);
     
-    for g_con = g
-        g_eval(q) = eval(subs(g_con, x.', x_k));
-        q = q + 1;
-    end
-    
-    % If the optimum is found -> stop iterating
-    [g_active, idx]  = maxk(g_eval,nx);
+    % Calculate Lagrangian multipliers
+    h_k = g_k(h_idx);
+    lambda_k = double(lambda_k + 1/delta*double(h_k));
     
     % Compute Hessian and Jacobian matrices for SQP
-    h = g(h_idx);
     ddhdxx = ddgdxx(:,:,h_idx);
     W = ddfdxx;
     for i = 1:nh
@@ -87,13 +90,8 @@ for p = 1:Niter
     
     % Evaluate matrices at the current point
     dfdx_k = subs(dfdx, x.', x_k);
-    h_k = subs(h, x.', x_k);
     W_k = subs(W, x.', x_k);
     A_k = subs(A, x.', x_k); 
-    
-    % Calculate Lagrangian multipliers
-    lambda_k = double(lambda_k + 1/delta*double(h_k));
-    % any update for lambda seems to be good; something is wrong!
     
     % Evaluate matrices with lambda
     W_eval = double(subs(W_k, lambda.', lambda_k));
@@ -104,7 +102,6 @@ for p = 1:Niter
     % Obtain update step
     [dx, sqp_obj, exitflag, output, lambda_next] = quadprog(W_eval, dfdx_eval.', [], [], A_eval, -h_eval, [], [], [], options);
     x_k = x_k + dx.';
-    %lambda_k = -lambda_next.eqlin.';
     
     if all(abs(dx) < 10^(-10))
         break;
@@ -115,8 +112,9 @@ for p = 1:Niter
     isposdef = all(ev> -10^(-14));
 
     % Compute objective
-    obj  = eval(subs(f_sym, x.', x_k));     
-    fprintf("%i: objective: %.3f,\t highest constraint: %.3f\n", p, obj, max(g_eval));
+    obj  = eval(subs(f_sym, x.', x_k));
+    g_eval = eval(subs(g, x.', x_k));
+    fprintf("%i: objective: %.3f,\t #constraints: %i,\t highest constraint: %.3f\n", p, obj, length(h_idx), max(g_eval));
 end
 
 fprintf('\n The minimum objective of %f with a max nominal stress of %f is reached for: \n', obj, 1 / obj)
