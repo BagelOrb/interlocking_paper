201e8109642fbe728a3bd91a4ff8f7fea5a23205
Nadine Duursma <n.a.duursma@outlook.com>
2021-07-02 15:40:41 +0200

    Obtain sensitivities for latex

--- analytical_optimization/sensitivities/sensitivities.m
+++ analytical_optimization/sensitivities/sensitivities.m
@@ -15,6 +15,33 @@ for i = 1:ng
     dLgdLx(:,i) = dLgdLx_i;
 end
 
+dgdx_latex = string([]);
+for j = 1:size(dgdx,2)
+    for i = 1:size(dgdx,1)
+        p = dgdx(i,j);
+        dgdx_latex(i,j) = string(latex(p));
+    end
+end
+
+dfdx_latex = string([]);
+for i = 1:length(dfdx)
+    p = dfdx(i);
+    dfdx_latex(i) = string(latex(p));
+end
+
+dLgdLx_latex = string([]);
+for j = 1:size(dLgdLx,2)
+    for i = 1:size(dLgdLx,1)
+        p = dLgdLx(i,j);
+        dLgdLx_latex(i,j) = string(latex(p));
+    end
+end
+
+dLfdLx_latex = string([]);
+for i = 1:length(dLfdLx)
+    p = dLfdLx(i);
+    dLfdLx_latex(i) = string(latex(p));
+end
 
 
 function [dLfdLx, dfdx] = sens(f, x)
@@ -28,4 +55,5 @@ function [dLfdLx, dfdx] = sens(f, x)
     for i = 1:n
         dLfdLx = [dLfdLx; simplify(x(i) / f * dfdx(i))];
     end
+    
 end
