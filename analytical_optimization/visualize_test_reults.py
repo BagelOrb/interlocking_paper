ad99e3d2ff9c169b20a9f2940b53b0e8e5727882
Tim Kuipers <t.kuipers@ultimaker.com>
2021-07-06 20:54:46 +0200

    visualize sampled data points in 3D

---
+++ analytical_optimization/visualize_test_reults.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+
+data_file = np.genfromtxt('../../../testing/results.csv', delimiter=';')
+
+wb = data_file[1:,2]
+va = data_file[1:,3]
+lmax = data_file[1:,4]
+hf = data_file[1:,5]
+stress = data_file[1:,13]  # divided by 5x5 cells
+stress2 = data_file[1:,15]  # divided by total cross section area
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+plot = ax.scatter(wb, va, hf, marker='o', edgecolors='black', c=stress, cmap=plt.get_cmap('rainbow'))
+
+fig.colorbar(plot, fraction=0.046*.5, pad=0.1)
+
+ax.set_xlabel('$w_b$')
+ax.set_ylabel('$v_a$')
+ax.set_zlabel('$h_f$')
+
+plt.show()
