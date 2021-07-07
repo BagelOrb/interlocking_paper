a66c0d231b925101a6af854cf16240e726c703bf
Tim Kuipers <t.kuipers@ultimaker.com>
2021-07-07 16:25:32 +0200

    account for extra line at start of csv

--- analytical_optimization/visualize_test_reults.py
+++ analytical_optimization/visualize_test_reults.py
@@ -6,12 +6,12 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 data_file = np.genfromtxt('../../../testing/results.csv', delimiter=';')
 
-wb = data_file[1:,2]
-va = data_file[1:,3]
-lmax = data_file[1:,4]
-hf = data_file[1:,5]
-stress = data_file[1:,13]  # divided by 5x5 cells
-stress2 = data_file[1:,15]  # divided by total cross section area
+wb = data_file[2:,2]
+va = data_file[2:,3]
+lmax = data_file[2:,4]
+hf = data_file[2:,5]
+stress = data_file[2:,13]  # divided by 5x5 cells
+stress2 = data_file[2:,15]  # divided by total cross section area
 
 
 fig = plt.figure()
