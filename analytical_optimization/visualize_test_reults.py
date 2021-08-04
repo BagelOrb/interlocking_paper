b11e1266483ad3b187993701bd5c593b3d42356a
Tim Kuipers <t.kuipers@ultimaker.com>
2021-08-04 15:04:16 +0200

    better straight sample points figure

--- analytical_optimization/visualize_test_reults.py
+++ analytical_optimization/visualize_test_reults.py
@@ -17,12 +17,21 @@ stress2 = data_file[2:,15]  # divided by total cross section area
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-plot = ax.scatter(wb, va, hf, marker='o', edgecolors='black', c=stress, cmap=plt.get_cmap('rainbow'))
+plot = ax.scatter(wb, va, hf, marker='o', edgecolors='black', c=stress, cmap=plt.get_cmap('jet'))
 
 fig.colorbar(plot, fraction=0.046*.5, pad=0.1)
 
 ax.set_xlabel('$w_b$')
 ax.set_ylabel('$v_a$')
 ax.set_zlabel('$h_f$')
+
+#ax.set_xticks(np.arange(wb.min(), wb.max(), .3))
+ax.set_xticks(np.unique(wb))
+ax.set_yticks(np.arange(va.min(), va.max(), .3), minor=True)
+ax.set_yticks(np.arange(va.min(), va.max(), .6), minor=False)
+#ax.set_yticks(np.unique(va))
+#ax.set_zticks(np.unique(hf))
+
+plt.margins(0)
 
 plt.show()
